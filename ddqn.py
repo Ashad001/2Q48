c1f84bef909ce4e51f5805019d83e8758a5c2dc1
Shaheer Ul Islam <shaheerislam.islam179@gmail.com>
2024-11-18 14:39:11 +0500

    pushing my agent. will explode if executed

--- ddqn.py
+++ ddqn.py
@@ -1,78 +1,67 @@
-import os
+#deleted your agent. 
+#my agent from valor 
+
 import numpy as np
-import tensorflow as tf
-from grid import Grid
-from utils.logger import *
+from collections import deque
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
 
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+import numpy as np
+from collections import deque
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
 
-# Logger setup
-episode_logger = get_logger("episodes_run", see_time=True)
-action_logger = get_logger("actions_run", see_time=False)
-reward_logger = get_logger("rewards_run", see_time=False)
-
-class DQNAgent:
-    def __init__(self, size, state_size, action_size):
-        self.size = size
+class Agent:
+    def __init__(self, state_size, action_size):
         self.state_size = state_size
         self.action_size = action_size
+        self.memory = deque(maxlen=2000)  # Replay memory buffer
+        self.gamma = 0.95  # Discount factor
+        self.epsilon = 1.0  # Exploration rate
+        self.epsilon_min = 0.01  # Minimum exploration rate
+        self.epsilon_decay = 0.995  # Exploration decay rate
+        self.learning_rate = 0.001  # Learning rate
         self.model = self._build_model()
-        self.env = Grid(size)
-
-        # Logging initialization
-        log_message("DQNAgent initialized for running", episode_logger)
+        self.filename = 'agentmodel.h5'
+        print(self.model)
 
     def _build_model(self):
-        model = tf.keras.Sequential()
-        model.add(tf.keras.layers.Dense(128, input_dim=self.state_size, activation='relu'))
-        model.add(tf.keras.layers.Dense(128, activation='relu'))
-        model.add(tf.keras.layers.Dense(self.action_size, activation='linear'))
-        model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), loss='mse')
-
-        log_message("Model built for running", episode_logger)
+        model = Sequential()
+        model.add(Dense(24, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(self.action_size, activation='linear'))
+        model.compile(loss='mse', optimizer=Adam(learning_rate=self.learning_rate))
         return model
 
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
     def act(self, state):
+        # if np.random.rand() <= self.epsilon:
+        #     return np.random.choice(self.action_size)
         act_values = self.model.predict(state)
-        action = np.argmax(act_values[0])
-        log_message(f"Action predicted: {action}", action_logger)
-        return action
-
-    def load(self, name):
-        self.model.load_weights(name)
-        log_message(f"Model weights loaded from {name}", episode_logger)
-
-    def run(self, episodes):
-        for e in range(episodes):
-            state = np.reshape(self.env.reset(), [1, self.state_size])
-            done = False
-            total_reward = 0
-            time_step = 0
-            while not done:
-                action = self.act(state)
-                next_state, reward, done = self.env.step(action)
-                total_reward += reward
-                next_state = np.reshape(next_state, [1, self.state_size])
-                state = next_state
-                
-                self.env.render()
-
-                if done:
-                    log_message(f"Episode {e + 1}/{episodes} finished with score {self.env.score} and reward {total_reward}", episode_logger)
-                    reward_logger.info(f"Episode {e + 1}, Reward: {total_reward}")
-                    break
-
-                time_step += 1
-
-if __name__ == "__main__":
-    size = 4
-    state_size = size * size
-    action_size = 4  # Up, Down, Left, Right
-    episodes = 10
-    agent = DQNAgent(size, state_size, action_size)
+        return np.argmax(act_values[0])
     
-    # Load the pre-trained model weights
-    agent.load("model.weights.h5")
-    
-    # Run the environment using the pre-trained model
-    agent.run(episodes)
+    def replay(self, batch_size):
+        if len(self.memory) < batch_size:
+            return
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            next_state = np.reshape(next_state, (1, self.state_size))
+            state = np.reshape(state, (1, self.state_size))
+            target = reward
+            if not done:
+                target = reward + self.gamma * np.amax(self.model.predict(next_state))
+            target_f = self.model.predict(state)
+            target_f[0][action] = target
+            self.model.fit(state, target_f, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def save(self,filename):
+        self.model.save_weights(filename)
+
+    def load(self, filename):
+        self.model.load_weights(filename)
